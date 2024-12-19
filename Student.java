e4a477fa339c7e7890c3d5d20d9beb1cf148819b
pradeep286 <pradeep30014@gmail.com>
2024-12-19 13:28:26 +0530

    push

---
+++ Student.java
@@ -0,0 +1,19 @@
+xclass Student{
+String name;
+int age;
+
+Student(String name, int age){
+    this.name = name;
+    this.age = age;
+    System.out.println(this.name);
+}
+Student(Student student){
+    this.name = student.name;
+    this.age = student.age;
+    System.out.println(this.name);
+}
+public static void main (String args[]){
+    Student student1 = new Student("arshad",20);
+    Student student2 = new Student(student1);
+}
+}
